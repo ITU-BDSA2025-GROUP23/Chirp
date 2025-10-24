42b0eb1e4012b21ab2998234db0cdae8a7e587e4
MCITU <koldenborgmads@gmail.com>
2025-10-24 10:01:22 +0200

    create data model (#25)

---
+++ src/Chirp.Razor/DataModel/Author.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chirp.Razor.DataModel;
+
+public class Author
+{
+    public required int id  { get; set; }
+    public required string Username { get; set; }
+    
+    public required string Email { get; set; }
+    
+    public required ICollection<Cheep>  Cheeps { get; set; }
+}
