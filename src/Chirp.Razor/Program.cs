43da8d794fea6463e49d240eda998f9e51de5395
MCITU <koldenborgmads@gmail.com>
2025-10-31 14:39:40 +0100

    implement seed
    Co-Authored-By: Rasmus <ratn@itu.dk>

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -2,6 +2,7 @@ using Chirp.Razor.DataModel;
 using Database;
 using Microsoft.EntityFrameworkCore;
 using Chirp.Razor;
+using Chirp.Razor.db;
 
 //Region build app and register DbContext
 var builder = WebApplication.CreateBuilder(args);
@@ -26,17 +27,7 @@ var app = builder.Build();
 	
  if (!context.Authors.Any() && !context.Cheeps.Any())
     {
-        var alice = new Author {  AuthorId = 13, Name = "alice", Email = "alice@example.com", Cheeps = new List<Cheep>() };
-        var bob   = new Author {  AuthorId = 14, Name = "bob",   Email = "bob@example.com",   Cheeps = new List<Cheep>() };
-
-        context.Authors.AddRange(alice, bob);
-        context.SaveChanges();
-
-        context.Cheeps.AddRange(
-            new Cheep {CheepId = 700, AuthorId = alice.AuthorId, Author= alice,  Text = "Hello Chirp!",        TimeStamp = DateTime.UtcNow},
-            new Cheep {CheepId = 701,  AuthorId = bob.AuthorId, Author= bob, Text = "Second cheep here!",  TimeStamp = DateTime.UtcNow}
-        );
-        context.SaveChanges();
+        DbInitializer.SeedDatabase(context);
     }
 
 }
