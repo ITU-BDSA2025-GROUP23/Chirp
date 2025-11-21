aaef059bbe9291a8916f6c39f6430ecd4c8d7a41
MCITU <koldenborgmads@gmail.com>
2025-11-21 09:52:06 +0100

    add TestEntityMapping

--- test/ChirpPaginationTest.cs
+++ test/ChirpPaginationTest.cs
@@ -68,4 +68,34 @@ public class ChirpPaginationTest: IClassFixture<TestingWebApplicationFactory>
         //Assert
         Assert.Equal(resultA, resultB);
     }
+	[Fact]
+    public async Task TestEntityMapping()
+    {
+        //Arrange 
+        using var connection = new SqliteConnection("Filename=:memory:");
+        await connection.OpenAsync();
+        var builder = new DbContextOptionsBuilder<ChatDBContext>().UseSqlite(connection);
+
+        using var context = new ChatDBContext(builder.Options);
+        await context.Database.EnsureCreatedAsync(); 
+
+        ICheepRepository repository = new CheepRepository(context);
+	
+		string username = "username";
+        string email = "email@itu.dk";
+        string text = "Testing if DTO gets mapped correctly";
+
+        //act
+		repository.CreateCheep(username,  email, text);
+		var result = repository.GetPaginatedCheepsDTO(1,10,"username");
+		var cheep = context.Cheeps.First();
+		var timestamp = cheep.TimeStamp.ToString("yyyy-MM-dd HH:mm");
+
+        //Assert
+		Assert.Equal(1, result[0].Id);
+		Assert.True(result[0].Id != 0);
+		Assert.Equal(text, result[0].Text);
+		Assert.Equal(timestamp, result[0].TimeIso);
+		Assert.Equal(username, result[0].AuthorName);	
+    }
 }
