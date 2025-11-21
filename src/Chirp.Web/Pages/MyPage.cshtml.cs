d0f65a61ce51d76472ecc7296576995b9785f616
MCITU <koldenborgmads@gmail.com>
2025-11-21 16:16:11 +0100

    add mypage

---
+++ src/Chirp.Web/Pages/MyPage.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using Chirp.Core.DTOs;
+using Chirp.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace Chirp.Web.Pages;
+
+public class MyPageModel : PaginationModel
+{
+    public IActionResult OnGet()
+    {
+            if (!(User.Identity?.IsAuthenticated ?? false))
+                return RedirectToPage("/Public");   
+
+            var name = User.Identity!.Name!;
+            return RedirectToPage("/UserTimeline", new { author = name });
+    }
+}
+
