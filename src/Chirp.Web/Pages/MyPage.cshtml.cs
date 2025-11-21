0bfc5ac59aebb38903aa58ada251d7fa80eca8ca
MCITU <koldenborgmads@gmail.com>
2025-11-21 18:20:02 +0100

    working post option
    Co-authored-by: Rasmus <ratn@itu.dk>

--- src/Chirp.Web/Pages/MyPage.cshtml.cs
+++ src/Chirp.Web/Pages/MyPage.cshtml.cs
@@ -9,16 +9,24 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 
 namespace Chirp.Web.Pages;
-
 public class MyPageModel : PaginationModel
 {
+
+    private readonly ICheepRepository _repository;
+
+    public MyPageModel(ICheepRepository repository) => _repository = repository;
     public IActionResult OnGet()
     {
             if (!(User.Identity?.IsAuthenticated ?? false))
                 return RedirectToPage("/Public");   
-
-            var name = User.Identity!.Name!;
-            return RedirectToPage("/UserTimeline", new { author = name });
+            
+            var email = User.Identity!.Name!;
+            
+            var author = _repository.GetAuthorByEmail(email);
+            
+            var authorName = author?.Name ?? email;
+            
+            return RedirectToPage("/UserTimeline", new { author = authorName });
     }
 }
 
--- src/Chirp.Web/Pages/PostCheep.cshtml.cs
+++ src/Chirp.Web/Pages/PostCheep.cshtml.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Chirp.Core.DTOs;
 using Chirp.Infrastructure.Repositories;
-namespace Chirp.Web.Pages;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
+namespace Chirp.Web.Pages;
 
 public class  PostCheepModel : PageModel //PaginationModel?
 {
@@ -14,12 +16,27 @@ public class  PostCheepModel : PageModel //PaginationModel?
     public List<CheepDTO> Cheeps { get; set; } = new();
     public PostCheepModel(ICheepRepository repository) => _repository = repository;
     
-    public IActionResult OnPost()
+   /* public IActionResult OnPost()
     {
         if (!ModelState.IsValid)
         {
             return Page();
         }
         return RedirectToPage("/Public");
+    }*/
+
+    public IActionResult OnPost()
+    {
+        var email = User.Identity.Name;
+        var userName = email;
+        var text = Message;
+
+        /*if (userName.Name == null)
+        {
+            return RedirectToPage("/");
+        }*/
+        
+        _repository.CreateCheep(userName,email, text);
+        return RedirectToPage("/MyPage");
     }
 }
