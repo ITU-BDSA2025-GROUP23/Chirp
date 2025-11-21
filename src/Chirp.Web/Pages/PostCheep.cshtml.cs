58913f9230a01a4e26f90e8f79916c7a7b880dd8
MCITU <koldenborgmads@gmail.com>
2025-11-21 16:58:07 +0100

    set up post cheep
    Co-authored-by: Rasmus <ratn@itu.dk>

---
+++ src/Chirp.Web/Pages/PostCheep.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Chirp.Core.DTOs;
+using Chirp.Infrastructure.Repositories;
+namespace Chirp.Web.Pages;
+
+
+public class  PostCheepModel
+{
+    [BindProperty] public string Message { get; set; }
+    
+    private readonly ICheepRepository _repository;
+    
+    public List<CheepDTO> Cheeps { get; set; } = new();
+    public PostCheepModel(ICheepRepository repository) => _repository = repository;
+    
+    /*public IActionResult OnPost()
+    {
+        
+    }
+    */
+}
