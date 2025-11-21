a5d3859f7083d0c8880ebf64bd66c2f1ed7c723b
Arnil <Andersgrangaard@gmail.com>
2025-11-21 13:56:39 +0100

    Add OAuth + Identity integration

--- src/Chirp.Infrastructure/DataModel/ChatDBContext.cs
+++ src/Chirp.Infrastructure/DataModel/ChatDBContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Infrastructure.DataModel;
 
-public class ChatDBContext : DbContext
+public class ChatDBContext : IdentityDbContext<IdentityUser>
 {
     public ChatDBContext(DbContextOptions<ChatDBContext> options) :
         base(options) { }
--- src/Chirp.Infrastructure/Migrations/20251121111344_InitialCreate.Designer.cs
+++ src/Chirp.Infrastructure/Migrations/20251121111344_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Chirp.Infrastructure.DataModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chirp.Infrastructure.Migrations
 {
     [DbContext(typeof(ChatDBContext))]
-    partial class ChatDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251121111344_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
