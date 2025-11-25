9d4a2b4eeff23482c69a1ecb0aa7258a60358ba4
MCITU <koldenborgmads@gmail.com>
2025-11-25 16:11:26 +0100

    add LogInBeforePost test

--- src/Chirp.Web/Pages/PostCheep.cshtml.cs
+++ src/Chirp.Web/Pages/PostCheep.cshtml.cs
@@ -11,20 +11,12 @@ public class  PostCheepModel : PageModel //PaginationModel?
 {
     [BindProperty] public string Message { get; set; }
     
+    public bool CanPost => User?.Identity?.IsAuthenticated ?? false;
     private readonly ICheepRepository _repository;
     
     public List<CheepDTO> Cheeps { get; set; } = new();
     public PostCheepModel(ICheepRepository repository) => _repository = repository;
     
-   /* public IActionResult OnPost()
-    {
-        if (!ModelState.IsValid)
-        {
-            return Page();
-        }
-        return RedirectToPage("/Public");
-    }*/
-
     public IActionResult OnPost()
     {
         var email = User.Identity.Name;
---
+++ test/ChirpUITest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Chirp.test;
+
+public class ChirpUITest : IClassFixture<TestingWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+
+    public ChirpUITest(TestingWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task LogInBeforePost()
+    {
+        // Arrange + Act
+        var response = await _client.GetAsync("/postCheep");
+        var html = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Contains("YOU HAVE TO LOG IN TO USE THIS FUNCTION", html);
+    }
+}
