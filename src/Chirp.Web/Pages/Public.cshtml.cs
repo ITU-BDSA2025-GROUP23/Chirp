9dc11f14c76f9cd1264dd572136dc17a93623930
Rasmus Tornvig Nordquist <ratn@itu.dk>
2025-12-01 21:49:46 +0100

    Added follow and unfollow implementation

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -4,15 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using Chirp.Core.DTOs;
 using Chirp.Infrastructure.Repositories;
+using Chirp.Infrastructure.DataModel;
 
 namespace Chirp.Web.Pages;
 
 public class PublicModel : PaginationModel
 {
-
     private readonly ICheepRepository _service;
-
     public List<CheepDTO> Cheeps { get; set; } = new();
+    public Author? CurrentUser { get; set; }
     public int TotalCheeps { get; private set; }
     public int NumberOfCheeps => Cheeps.Count;
     public int TotalPages => (int)System.Math.Ceiling((double)TotalCheeps / PageSize);
@@ -24,6 +24,11 @@ public class PublicModel : PaginationModel
 
     public IActionResult OnGet([FromQuery(Name = "page")] int page = 1)
     {
+        if (User.Identity?.IsAuthenticated == true)
+        {
+            CurrentUser = _service.GetAuthorByName(User.Identity.Name);
+        }
+        
         if (int.TryParse(Request.Query["p"], out var p) && p > 0) CurrentPage = p;
         else
             CurrentPage = 1;
@@ -47,8 +52,23 @@ public class PublicModel : PaginationModel
             currentUser.Follow(authorToFollow);
             _service.SaveChanges();
         }
-        
-        Console.WriteLine($"{currentUser.Name} now follows: {string.Join(", ", currentUser.Following.Select(a => a.Name))}");
+
+        return RedirectToPage("/Public", new { p = CurrentPage });
+    }
+
+    public IActionResult OnPostUnfollow(string authorName)
+    {
+        var currentUser = _service.GetAuthorByName(User.Identity.Name);
+        var authorToUnfollow = _service.GetAuthorByName(authorName);
+
+        if (currentUser != null && authorToUnfollow != null && currentUser.AuthorId != authorToUnfollow.AuthorId)
+        {
+            if (currentUser.Following.Any(a => a.AuthorId == authorToUnfollow.AuthorId))
+            {
+                currentUser.Following.Remove(authorToUnfollow);
+                _service.SaveChanges();
+            }
+        }
 
         return RedirectToPage("/Public", new { p = CurrentPage });
     }
